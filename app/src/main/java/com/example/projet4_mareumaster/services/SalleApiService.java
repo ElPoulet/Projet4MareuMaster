2b4f1a6bff89f286e101cb1beddf5728f6293027
ElPoulet <55815422+ElPoulet@users.noreply.github.com>
2020-03-29 11:55:10 +0200

    nouvelle version

--- app/src/main/java/com/example/projet4_mareumaster/services/SalleApiService.java
+++
@@ -1,9 +0,0 @@
-package com.example.projet4_mareumaster.services;
-
-import com.example.projet4_mareumaster.model.Salle;
-
-import java.util.List;
-
-public interface SalleApiService {
-    List<Salle> getSalles();
-}
